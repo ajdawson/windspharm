ajdawson/windspharm

commit 5a70842352cea42eeee2163abce492b4501d4614

Corrected bug in planetary vorticity function.
The latitudes used to compute planetary vorticity were incorrect.
These have been fixed.

Lib/standard.py
@@ -221,11 +221,10 @@ class VectorWind(object):
             omega = 7.292e-05
         nlat = self.s.nlat
         if nlat % 2:
-            # Poles included.
-            lat = np.linspace(-90, 90, nlat)
+            lat = np.linspace(90, -90, nlat)
         else:
             dlat = 180. / nlat
-            lat = np.linspace(-90+dlat, 90, dlat)
+            lat = np.arange(90-dlat/2., -90, dlat)
         try:
             cp = 2. * omega * np.sin(np.deg2rad(lat))
         except TypeError, ValueError:
@@ -429,7 +428,6 @@ w.helmholtz(truncation=13)
         psigrid, chigrid = self.s.getpsichi(self.u, self.v, ntrunc=truncation)
         psispec = self.s.grdtospec(psigrid)
         vpsi, upsi = self.s.getgrad(psispec)
-#>>>>>>># Is there a step missing here that inverts the sign?
         return -upsi, vpsi
 
     def gradient(self, chi, truncation=None):

commit a2881f2c435f0fa1940679ec8469315a5b971bef

Planetary vorticity bug fixes. Closes #5.
Support for computing planetary vorticity for Gaussian grids was
omitted. This is now fixed. Planetary vorticity for offset grids
remained broken after commit 5a70842352cea42eeee2163abce492b4501d4614
so this is also fixed.

Lib/standard.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 import numpy as np
-from spharm import Spharmt
+from spharm import Spharmt, gaussian_lats_wts
 
 
 class VectorWind(object):
@@ -96,9 +96,10 @@ class VectorWind(object):
             raise ValueError('nlon must be >= 4 and nlat must be >= 3')
         try:
             # Create a Spharmt object to do the computations.
-            self.s = Spharmt(nlon, nlat, gridtype=gridtype.lower())
+            self.gridtype = gridtype.lower()
+            self.s = Spharmt(nlon, nlat, gridtype=self.gridtype)
         except ValueError:
-            if gridtype.lower() not in ('regular', 'gaussian'):
+            if self.gridtype not in ('regular', 'gaussian'):
                 err = 'invalid grid type: {0:s}'.format(repr(gridtype))
             else:
                 err = 'invalid input dimensions'
@@ -220,11 +221,14 @@ class VectorWind(object):
             # Define the Earth's angular velocity.
             omega = 7.292e-05
         nlat = self.s.nlat
-        if nlat % 2:
-            lat = np.linspace(90, -90, nlat)
+        if self.gridtype == 'gaussian':
+            lat, wts = gaussian_lats_wts(nlat)
         else:
-            dlat = 180. / nlat
-            lat = np.arange(90-dlat/2., -90, dlat)
+            if nlat % 2:
+                lat = np.linspace(90, -90, nlat)
+            else:
+                dlat = 180. / nlat
+                lat = np.arange(90-dlat/2., -90, -dlat)
         try:
             cp = 2. * omega * np.sin(np.deg2rad(lat))
         except TypeError, ValueError: