299f7644e90bd1e14c20abfdf3d5f9dea72a67cb
Andrew Dawson <ajdawson@acm.org>
2013-08-14 15:22:24 +0100

    DOC - Changed iris examples to use iris plotting features.

--- examples/iris/rws_example.py
+++ examples/iris/rws_example.py
@@ -5,13 +5,14 @@ This example uses the iris interface.
 """
 import warnings
 
-import numpy as np
+import cartopy.crs as ccrs
+import iris
+import iris.plot as iplt
+from iris.coord_categorisation import add_month
 import matplotlib as mpl
 mpl.rcParams['mathtext.default'] = 'regular'
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap, addcyclic
-import iris
-from iris.coord_categorisation import add_month
+import numpy as np
 
 from windspharm.iris import VectorWind
 from windspharm.examples import example_data_path
@@ -24,6 +25,8 @@ with warnings.catch_warnings():
     warnings.simplefilter('ignore', UserWarning)
     uwnd = iris.load_cube(example_data_path('uwnd_mean.nc'))
     vwnd = iris.load_cube(example_data_path('vwnd_mean.nc'))
+uwnd.coord('longitude').circular = True
+vwnd.coord('longitude').circular = True
 
 # Create a VectorWind instance to handle the computations.
 w = VectorWind(uwnd, vwnd)
@@ -39,6 +42,7 @@ etay.units = 'm**-1 s**-1'
 
 # Combine the components to form the Rossby wave source term.
 S = eta * -1. * div - uchi * etax + vchi * etay
+S.coord('longitude').attributes['circular'] = True
 
 # Pick out the field for December at 200 hPa.
 time_constraint = iris.Constraint(month='Dec')
@@ -46,17 +50,12 @@ add_month(S, 'time')
 S_dec = S.extract(time_constraint)
 
 # Plot Rossby wave source.
-m = Basemap(projection='cyl', resolution='c', llcrnrlon=0, llcrnrlat=-90,
-            urcrnrlon=360.01, urcrnrlat=90)
-lons, lats = S_dec.coord('longitude'), S_dec.coord('latitude')
-S_dec, lonsc = addcyclic(S_dec.data, lons.points)
-x, y = m(*np.meshgrid(lonsc, lats.points))
 clevs = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
-m.contourf(x, y, S_dec*1e11, clevs, cmap=plt.cm.RdBu_r, extend='both')
-m.drawcoastlines()
-m.drawparallels((-90, -60, -30, 0, 30, 60, 90), labels=[1,0,0,0])
-m.drawmeridians((0, 60, 120, 180, 240, 300, 360), labels=[0,0,0,1])
-plt.colorbar(orientation='horizontal')
+ax = plt.subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
+fill = iplt.contourf(S_dec*1e11, clevs, cmap=plt.cm.RdBu_r, extend='both')
+ax.coastlines()
+ax.gridlines()
+plt.colorbar(fill, orientation='horizontal')
 plt.title('Rossby Wave Source ($10^{-11}$s$^{-1}$)', fontsize=16)
 plt.show()
 
