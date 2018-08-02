f1cb0e3e462158fa566d832223d6815d22a65682
Andrew Dawson <ajdawson@acm.org>
2018-08-02 20:20:50 +0100

    Copyright catchup

--- lib/windspharm/tests/test_solution.py
+++ lib/windspharm/tests/test_solution.py
@@ -1,5 +1,5 @@
 """Test windspharm computations against reference solutions."""
-# Copyright (c) 2012-2016 Andrew Dawson
+# Copyright (c) 2012-2018 Andrew Dawson
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
