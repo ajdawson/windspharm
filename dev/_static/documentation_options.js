350ab597667691206541d7587fd7617f9626714e
Travis CI User <travis@example.org>
2018-08-20 20:49:42 +0000

    Update docs after building Travis build 338 of ajdawson/windspharm
    The docs were built from the branch 'master' against the commit
    15a4caab461f30dc65b7471791d1b987433f272b.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ajdawson/windspharm/jobs/418408947.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0+24.g3d40634',
+    VERSION: '1.6.0+27.g15a4caa',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
